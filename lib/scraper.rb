68cd5e6b2c7bad3de937b6dc1c41eec3c1a9a110
Eddie Melo <heriberto@melo.nyc>
2018-01-11 22:02:41 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -31,7 +31,7 @@ class Scraper
     learnco = Nokogiri::HTML(html)
     learnco.css("div.social-icon-container a").each do |social_media|
       social_media.each do |link|
-        puts links.value
+        puts link.attribute("href").value
       end
     end
   end
