19aa8b3f072052f7c6c83e0cd1cdffd53b9f719b
Eddie Melo <heriberto@melo.nyc>
2018-01-11 13:19:44 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -33,8 +33,9 @@ class Scraper
 
   def self.strip_url(url)
     if (url.include? "https://www.")
-      url.gsub!(/^https?\:\/\/(www.)?/,'')
-      url.split('.')[-1]
+      #url.gsub!(/^https?\:\/\/(www.)?/,'')
+      #url.split('.')[-1]
+      uri.parse(url).path[1]
       puts "#{url}"
     elsif (url.include? "https://")
       url.gsub!(/^https?\:\/\//,'')
